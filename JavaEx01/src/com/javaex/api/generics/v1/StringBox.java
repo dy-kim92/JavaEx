0f7d0c8a8e09a002550fdf5d499970089e14d37f
김동연 <hsg99091@gmail.com>
2021-03-08 15:47:13 +0900

    Collection Framework (Vector)

---
+++ JavaEx01/src/com/javaex/api/generics/v1/StringBox.java
@@ -0,0 +1,16 @@
+package com.javaex.api.generics.v1;
+
+public class StringBox {
+	
+	private String content;
+
+	//	getter, setter
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	}
