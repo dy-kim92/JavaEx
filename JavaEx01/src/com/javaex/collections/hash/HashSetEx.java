3d580072dc9cc28216b85fe7261883812b73fa42
USER <USER@DESKTOP-6H3BD7A>
2021-03-09 10:46:13 +0900

    Stack, Queue, HashSet basic

---
+++ JavaEx01/src/com/javaex/collections/hash/HashSetEx.java
@@ -0,0 +1,44 @@
+package com.javaex.collections.hash;
+
+import java.util.HashSet;
+
+public class HashSetEx {
+	public static void main(String[] args) {
+		
+		basicHashSetEx();
+		
+	}
+	
+	private static void basicHashSetEx() {
+		
+		HashSet<String> hs = new HashSet<>();
+		
+		//	요소 추가
+		hs.add("Java");
+		hs.add("C");
+		hs.add("C++");
+		hs.add("Python");
+		
+		System.out.println("HashSet : " + hs);			//	순서는 유지하지 않음
+		System.out.println("요소의 수 : " + hs.size());
+		
+		hs.add("Java");
+		System.out.println("HashSet : " + hs);			//	중복을 허용하지 않음
+		
+		//	요소의 포함 여부 확인
+		System.out.println("contains Java ? " + hs.contains("Java"));
+		System.out.println("contains Linux ? " + hs.contains("Linux"));
+		
+		System.out.println("Hash set : " + hs);
+		
+		//	요소의 삭제
+		hs.remove("C++");
+		System.out.println("Hash set : " + hs);
+		
+		//	비우기
+		hs.clear();
+		System.out.println("Hash set : " + hs);
+		
+	}
+
+}
