69f8135b44f42e6c95e1967739dcd5d267eab426
USER <USER@DESKTOP-6H3BD7A>
2021-03-10 14:19:18 +0900

    Scanner 클래스

---
+++ JavaEx01/src/com/javaex/io/charstream/ScannerEx.java
@@ -0,0 +1,44 @@
+package com.javaex.io.charstream;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ScannerEx {
+	static final String rootPath = System.getProperty("user.dir") + "\\files\\";
+	static final String filename = rootPath + "thieves.txt";
+	
+	public static void main(String[] args) {
+		//	Scanner 는 단순 키보드 입력을 받기 위한 클래스가 아님
+		//	다양한 입력 소스로부터 데이터를 입력 받는 범용 클래스
+		Scanner sc = null;
+		File f = new File(filename);
+		
+		try {
+			sc = new Scanner(f);	//	파일로부터
+			
+			String name;
+			float height;
+			float weight;
+			
+			//	스캐너의 EOS(End of Stream)은 체크를 해 주자
+			while(sc.hasNextLine()) {	//	뒤에 남아있는 라인이 있는가?
+				name = sc.next();
+				height = sc.nextFloat();
+				weight = sc.nextFloat();
+				
+				System.out.printf("%s, 키 : %f, 체중 : %f\n", name, height, weight);
+				//	다음 행으로 이동
+				sc.nextLine();
+			}
+		}catch (FileNotFoundException e) {
+			System.err.println("파일을 찾을 수 없습니다.");
+		}finally {
+			try {
+				sc.close();
+			}catch(Exception e) {}
+		}
+		
+	}
+
+}
