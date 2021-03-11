a6a064ba79388388ef83c69c76f1fd8e0be93f03
김동연 <hsg99091@gmail.com>
2021-03-11 11:21:30 +0900

    Echo Back

--- JavaEx01/src/com/javaex/network/echoserver/Server.java
+++ JavaEx01/src/com/javaex/network/echoserver/Server.java
@@ -1,6 +1,14 @@
 package com.javaex.network.echoserver;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -29,6 +37,41 @@ public class Server {
 			InetSocketAddress socketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();	//	원격지 소켓의 주소 확인
 			System.out.println("SERVER : [클라이언트가 연결되었습니다]");
 			System.out.println("   클라이언트 : " + socketAddress.getAddress() + " : " + socketAddress.getPort());
+			
+			
+			//	메세지 수신
+			InputStream is = socket.getInputStream();
+			Reader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			
+			//	Echo Back 을 위한 OutputStream
+			OutputStream os = socket.getOutputStream();
+			Writer osw = new OutputStreamWriter(os, "UTF-8");
+			BufferedWriter bw = new BufferedWriter(osw);
+			
+			//	메세지 읽어오기
+			String message;
+			
+			while(true) {
+				message = br.readLine();
+				
+				if(message == null) {	//	읽을 메세지 없음
+					System.out.println("SERVER : 접속을 종료합니다.");
+					break;
+				}
+				System.out.println("SERVER : [수신 메세지] : " + message);
+				
+				//	Echo Back 메세지 전송
+				message = "Echo Back - " + message;
+				System.out.println("SERVER : [Echo Back] : " + message);
+				bw.write(message);
+				bw.newLine();
+				bw.flush();
+				
+			}
+			
+			bw.close();
+			br.close();
 			
 			//	후처리
 			System.out.println("SERVER : [서버를 종료합니다]");
--- JavaEx01/src/com/javaex/practice02/Problem04.java
+++ JavaEx01/src/com/javaex/practice02/Problem04.java
@@ -1,40 +1,58 @@
 package com.javaex.practice02;
 
+import java.util.Scanner;
+
 public class Problem04 {
 	public static void main(String[] args) {
 		
 		int[] lotto = new int[6];
 		int idx = 0;	// 인덱스 변수
 		
-		while(idx < lotto.length) {
-			//	난수발생
-			int choice = (int)(Math.random() * 45) + 1;
-			//	중복 데이터 체크
-			boolean isDuplicated = false;
-			
-			if(idx != 0) {
-				//	중복 체크
-				for(int i = 0; i < idx; i++) {
-					//	내부 값 체크
-					if(lotto[i] == choice) {
-						//	중복
-						isDuplicated = true;
-						break;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("몇조합 생성 ? ");
+		int num = sc.nextInt();
+		
+		int[][] result = new int[num][6];
+		
+		for (int k = 0; k < num; k++) {
+			while(idx < lotto.length) {
+				//	난수발생
+				int choice = (int)(Math.random() * 45) + 1;
+				//	중복 데이터 체크
+				boolean isDuplicated = false;
+				
+				if(idx != 0) {
+					//	중복 체크
+					for(int i = 0; i < idx; i++) {
+						//	내부 값 체크
+						if(lotto[i] == choice) {
+							//	중복
+							isDuplicated = true;
+							break;
+						}
 					}
 				}
-			}
-			
-			if(!isDuplicated) {
-				//	중복되지 않을 때 코드 수행
-				lotto[idx] = choice;
-				idx++;
+				
+				if(!isDuplicated) {
+					//	중복되지 않을 때 코드 수행
+					lotto[idx] = choice;
+					idx++;
+				}
 			}
 		}
 		
-		for(int value: lotto) {
-			System.out.print(value + " ");
+		for (int i = 0; i < num; i++) {
+			for(int value: lotto) {
+				result[i][value] = value;
+			}
 		}
-		System.out.println();
+		
+		for (int i = 0; i < num; i++) {
+			for (int value: lotto) {
+				System.out.print(value + " ");
+			}
+			System.out.println();
+		}
 		
 	}
 	
