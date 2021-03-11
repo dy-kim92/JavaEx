1e15c8a53b4cfc513df2dbffb0829d568b4795dc
김동연 <hsg99091@gmail.com>
2021-03-11 11:47:02 +0900

    Echo Server Final 	- Multithread Server

--- JavaEx01/src/com/javaex/network/echoserver/Server.java
+++ JavaEx01/src/com/javaex/network/echoserver/Server.java
@@ -1,14 +1,6 @@
 package com.javaex.network.echoserver;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -31,9 +23,16 @@ public class Server {
 			System.out.println("SERVER : [연결을 기다립니다]");
 			
 			//	연결 대기
-			Socket socket = serverSocket.accept();
+			while(true) {
+				Socket socket = serverSocket.accept();
+				Thread thread = new ServerThread(socket);
+				thread.start();
+			}
 			
+			
+			// ---- Working Thread Logic
 			//	클라이언트 정보 확인
+			/*
 			InetSocketAddress socketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();	//	원격지 소켓의 주소 확인
 			System.out.println("SERVER : [클라이언트가 연결되었습니다]");
 			System.out.println("   클라이언트 : " + socketAddress.getAddress() + " : " + socketAddress.getPort());
@@ -72,9 +71,12 @@ public class Server {
 			
 			bw.close();
 			br.close();
+			*/
+			// ---- Working Thread Logic End
+			
 			
 			//	후처리
-			System.out.println("SERVER : [서버를 종료합니다]");
+//			System.out.println("SERVER : [서버를 종료합니다]");
 		}catch (Exception e) {
 			e.printStackTrace();
 		}finally {
